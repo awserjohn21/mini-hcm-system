107566a37db038f347b79fe4dcf062f5b249e065
John Carlo Dote <johncarlo.dote@elinnovtech.com>
2026-05-31 23:18:22 +0800

    Refactor Firebase Admin SDK init to use env vars and remove serviceAccountKey.json

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,8 +1,24 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../serviceAccountKey.json");
+
+// Check if all necessary environment variables are set
+if (
+  !process.env.FIREBASE_PROJECT_ID ||
+  !process.env.FIREBASE_CLIENT_EMAIL ||
+  !process.env.FIREBASE_PRIVATE_KEY
+) {
+  console.error("Missing Firebase service account environment variables.");
+  process.exit(1);
+}
+
+// Construct the service account object from environment variables
+const serviceAccountConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccountConfig),
 });
 
 var db = admin.firestore();
